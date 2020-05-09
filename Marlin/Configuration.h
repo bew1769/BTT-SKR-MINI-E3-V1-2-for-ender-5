cb66c1b92c02920b959075b1265aaa72480a7f3d
Bryan <bew1769@gmail.com>
2020-05-09 14:35:58 -0400

    Fixed unload

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -561,7 +561,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 200
+#define EXTRUDE_MAXLENGTH 550
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
