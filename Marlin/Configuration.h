3cb610f6d03ed55cdfad60d9ee35ec96126b288f
Bryan <bew1769@gmail.com>
2021-01-22 17:56:28 -0500

    kp

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -490,9 +490,9 @@
 // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it // Creality Ender-5
 
 // Creality Ender-5
-#define DEFAULT_Kp 21.73
-#define DEFAULT_Ki 1.54
-#define DEFAULT_Kd 76.55
+#define DEFAULT_Kp 27.72
+#define DEFAULT_Ki 2.81
+#define DEFAULT_Kd 68.35
 // Ultimaker
 //#define DEFAULT_Kp 22.2
 //#define DEFAULT_Ki 1.08
@@ -545,9 +545,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 105.23
+  #define DEFAULT_bedKi 19.89
+  #define DEFAULT_bedKd 371.19
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
